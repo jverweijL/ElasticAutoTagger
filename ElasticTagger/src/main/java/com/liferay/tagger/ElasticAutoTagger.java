c5cbc53b0a1d627ca93fcaddcbbd34d168c6c321
Jan Verweij <jan.verweij@liferay.com>
2017-06-20 13:02:25 +0200

    the bare bone basic version

--- ElasticTagger/src/main/java/com/liferay/tagger/ElasticAutoTagger.java
+++ ElasticTagger/src/main/java/com/liferay/tagger/ElasticAutoTagger.java
@@ -14,10 +14,19 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.service.ServiceContext;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 /*import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;*/
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
 /*import java.net.URL;*/
 import java.util.List;
 
@@ -43,14 +52,19 @@ public class ElasticAutoTagger extends BaseModelListener<AssetEntry> {
 	//String autoTag = "autotag";
 	
 	private static Log _log = LogFactoryUtil.getLog(ElasticAutoTagger.class);
+	private static String USER_AGENT = "Mozarella/5.0";
 
 	@Override
 	public void onAfterCreate(AssetEntry entry) throws ModelListenerException {
 		super.onAfterCreate(entry);
 		
+		//TODO only autotag if there's an autotag tag
+		
 		if (entry.getClassName().equalsIgnoreCase(JournalArticle.class.getName())) {
-			//_log.error("TAG ARTICLE " + model.getTitle());
-			
+			_log.error("TAG ARTICLE " + entry.getTitle());
+			_log.error("TAG ARTICLE " + entry.getTitleCurrentValue());
+			_log.error("TAG ARTICLE " + entry.getTitleCurrentLanguageId());
+			//_log.error("TAG ARTICLE " + entry.getTitle(entry.getTitleCurrentLanguageId()));
 			/*Properties props = PropsUtil.getProperties();
 		      Enumeration<Object> e = props.keys();
 			
@@ -61,42 +75,16 @@ public class ElasticAutoTagger extends BaseModelListener<AssetEntry> {
 		    }*/
 			
 			try {
-				// doesn't seem to work due to OSGI no able to get to elastic libs
-				//_log.error("create ELS client");
-				//Client client = TransportClient.builder().build()
-				//        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9200));
-	
-				// on shutdown
-				//_log.error("close ELS client");
-				//client.close();
-				
-				/*String url = "http://localhost:9200/liferay-autotagger/my-type/_percolate'";
-	
-				URL obj = new URL(url);
-				HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-				con.setDoOutput(true);
-				con.setDoInput(true);
-				// optional default is GET
-				con.setRequestMethod("GET");
-				
-				OutputStream os = con.getOutputStream();
-				OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-				osw.write("{\"doc\" : {\"message\" : \"A new bonsai tree in the office\"}}");
-				osw.flush();
-				osw.close();
 				
 				
-				int responseCode = con.getResponseCode();
+				String[] tags = fetchTags(entry.getTitleCurrentValue());
 				
-				_log.error("Responsecode: " + responseCode);
-				
-				con.disconnect();*/			
-				
+				if (tags != null && tags.length > 0) {
 				ServiceContext serviceContext = new ServiceContext();
 				serviceContext.setCompanyId(entry.getCompanyId());
 				  
-				//TODO get suggestion(s) from Elastic or other service..
-				String tagName = "tennis";
+				//TODO loop over comma-separated items in case of multiple tags..
+				String tagName = tags[0];
 				_log.info("tagname: " + tagName);
 				AssetTag assetTag;
 				if (AssetTagLocalServiceUtil.hasTag(entry.getGroupId(), tagName)) {
@@ -107,34 +95,74 @@ public class ElasticAutoTagger extends BaseModelListener<AssetEntry> {
 				}
 				
 				
+				
+				
 				long[] tagIds = { assetTag.getTagId() };
 				
 				_log.info("tag: " + assetTag.getName());
 				
-				//AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
-				//assetEntryQuery.setAnyTagIds(tagIds);
-				//assetEntryQuery.setClassName(JournalArticle.class.getName());
-				//assetEntryQuery.setEnd(100);
-				//TODO limit to speicific user
-				//AssetEntry assetEntry = AssetEntryLocalServiceUtil.getEntry(model.getGroupId(), model.getUuid());
-				//List<AssetEntry> assetEntryList = AssetEntryLocalServiceUtil.getEntries(assetEntryQuery);
-				//for (AssetEntry assetEntry : assetEntryList) {
+				
 					_log.info("entryID: " + entry.getEntryId());
-				//}
-					//Here all the logic will go
-					//_log.error(assetEntry.getEntryId() + " | " + assetEntry.getPrimaryKey() + " | " + assetEntry.getUserId() + " | " + assetEntry.getGroupId());
-					//_log.info("PK:" + assetEntry.getClassPK() + " | " + model.getClassPK());
-					//assentEntry getClassPK() with JournalArticleLocalServiceUtil.getLatestArticle(classPK)
-					//_log.info("let's connect");
+				
 					// connect the tag to the asset
 					AssetTagLocalServiceUtil.addAssetEntryAssetTag(entry.getEntryId(), assetTag);
 					
-				//}
 				
-				//_log.error(entry.getContent());
+				}
 			} catch (Exception ex) {
 				_log.error("Foutje: " + ex.getMessage());
 			}
+		}
+	}
+
+	/**
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 * @throws ProtocolException
+	 */
+	private String[] fetchTags(String text) throws MalformedURLException, IOException, ProtocolException {
+		String url = "http://localhost:8080/tagger-service/rest/tags";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "text=" + URLEncoder.encode(text,"utf-8");
+		
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+		
+		
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		_log.error("AutoTagger: " + response.toString());
+		if (response != null && response.length() > 0) {
+			return response.toString().split(",");
+		} else {
+			return null;
 		}
 	}
 }
